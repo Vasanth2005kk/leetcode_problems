c48149167e446ec2f10849fcc936ffe91ba93b7c
Vasanth2005kk <vasanth2005kk@gmail.com>
2024-06-04 16:03:40 +0530

    two sum

--- tow_sum.py
+++ tow_sum.py
@@ -5,8 +5,9 @@ class Solution:
                 if nums[i]+nums[j]==target:
                     return [i,j]
                 
-nums=[2,7,11,15]
+nums=[2,11,15,7]
 target=9
 
 obj=Solution()
-obj.twoSum(nums=nums,target=target)
+output=obj.twoSum(nums=nums,target=target)
+print(output)
