f9ed18007115ad1060604cb51190055b8f5af662
vasanth <139038225+Vasanth2005kk@users.noreply.github.com>
2025-06-26 10:43:37 +0530

    Update 136. Single Number.py

--- 136. Single Number.py
+++ 136. Single Number.py
@@ -16,7 +16,15 @@ class Solution:
             for i in nums:
                 if i not in check_value:
                     return i
-                
+
+"""
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        set_value = set(nums)
+        for i in set_value:
+            if nums.count(i) == 1:
+                return i
+"""
 
 nums=[2,2,1]
 obj=Solution().singleNumber(nums=nums)
