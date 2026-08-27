d087af2669d6f987b8dfda833192685619b10525
Vasanth2005kk <vasanth2005kk@gmail.com>
2026-08-27 15:28:44 +0530

    219. Contains Duplicate II.py

---
+++ 219. Contains Duplicate II.py
@@ -0,0 +1,23 @@
+from typing import List
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        mp = {}
+
+        for i in range(len(nums)):
+            if nums[i] in mp and i - mp[nums[i]] <= k:
+                return True
+            mp[nums[i]] = i
+            # print(mp)
+        return False
+
+nums = [1,2,3,1]
+k = 3
+
+# nums = [1,0,1,1]
+# k = 1
+
+nums = [1,2,3,1,2,3]
+k = 2
+
+obj = Solution().containsNearbyDuplicate(nums,k)
+print(obj)
