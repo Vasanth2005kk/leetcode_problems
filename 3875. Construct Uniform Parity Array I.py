6992cf21649a931d9cc9db262e0410a0b078bc16
Vasanth2005kk <vasanth2005kk@gmail.com>
2026-09-02 10:44:28 +0530

    3875. Construct Uniform Parity Array I.py

---
+++ 3875. Construct Uniform Parity Array I.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        return True
+
+
+nums1 = [2,3]
+obj = Solution().uniformArray(nums1)
+
+print(obj)
