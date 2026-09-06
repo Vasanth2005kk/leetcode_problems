b45e2134628693403e14db32a8186d61f8698940
Vasanth2005kk <vasanth2005kk@gmail.com>
2026-09-07 00:13:00 +0530

    155. Min Stack.py

---
+++ 155. Min Stack.py
@@ -0,0 +1,50 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, value: int) -> None:
+        self.stack.append(value)
+
+        if len(self.minStack) == 0:
+            self.minStack.append(value)
+        else:
+            self.minStack.append(min(value, self.minStack[-1]))
+
+    def pop(self) -> None:
+        if self.stack:
+            self.stack.pop()
+            self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1] if self.stack else -1
+
+    def getMin(self) -> int:
+        return self.minStack[-1] if self.minStack else -1
+        
+
+
+
+# Your MinStack object will be instantiated and called as such:
+
+minStack = MinStack()
+minStack.push(-2)
+minStack.push(0)
+minStack.push(-1)
+minStack.getMin()
+minStack.pop()
+minStack.getMin()
+minStack.pop()
+minStack.getMin()
+minStack.pop()
+minStack.push(-2)
+minStack.push(1)
+minStack.push(-2)
+minStack.getMin()
+minStack.pop()
+minStack.top()
+minStack.getMin()
+minStack.pop()
+minStack.getMin()
+minStack.pop()
